fb45617223f9599639a0cbb89fd4f984a91407a3
YohnWang <347220312@qq.com>
2018-08-22 19:06:49 +0800

    change register interface

--- cpu/trap.c
+++ cpu/trap.c
@@ -6,16 +6,14 @@
 #include<task.h>
 #include<ds.h>
 
-extern void task_switch(xlen_t sp[]);
+extern void task_switch(struct regfiles *sp);
 
-void syscall_handler(xlen_t sp[])
+void syscall_handler(struct regfiles *sp)
 {
-#define sysarg(offset) (sp[9+(offset)])
-
-	switch(sysarg(0))
+	switch(sp->a0)
 	{
 		case SYS_EXIT:
-			exit(sysarg(1));
+			exit(sp->a1);
 			break;
 		case SYS_HALT:
 			
@@ -27,10 +25,9 @@ void syscall_handler(xlen_t sp[])
 			printf("system call error,no such sysnum\n");
 			exit(1);
 	}
-#undef sysarg
 }
 
-void time_handler(xlen_t sp[])
+void time_handler(struct regfiles *sp)
 {
 	//
 	extern struct prio sched;
@@ -57,7 +54,7 @@ void time_handler(xlen_t sp[])
 }
 
 
-xlen_t trap_handler(xlen_t mcause,xlen_t mepc,xlen_t sp[])
+xlen_t trap_handler(xlen_t mcause,xlen_t mepc,struct regfiles *sp)
 {
 
 	if(mcause<0)
@@ -71,7 +68,7 @@ xlen_t trap_handler(xlen_t mcause,xlen_t mepc,xlen_t sp[])
 			case 7://timer interrupt
 				
 				next_timecmp();
-				sp[31]=mepc;
+				sp->epc=mepc;
 				time_handler(sp);
 				break;
 			default:
@@ -84,7 +81,7 @@ xlen_t trap_handler(xlen_t mcause,xlen_t mepc,xlen_t sp[])
 		switch(mcause)
 		{
 			case 11://scall M-mode
-				sp[31]=mepc+4;
+				sp->epc=mepc+4;
 				syscall_handler(sp);
 				mepc+=4;
 				break;
--- cpu/trap.h
+++ cpu/trap.h
@@ -2,7 +2,7 @@
 #define _TRAP_H
 
 #include<cpu.h>
-
+#include<stdint.h>
 
 
 #ifndef NULL
@@ -10,9 +10,49 @@
 #endif
 
 
+
+struct regfiles
+{
+    intptr_t ra;    // Return address
+    intptr_t sp;    // Stack pointer
+    intptr_t gp;    // Global pointer
+    intptr_t tp;    // Thread pointer
+    intptr_t t0;    // Temporary
+    intptr_t t1;    // Temporary
+    intptr_t t2;    // Temporary
+    intptr_t s0;    // Saved register/frame pointer
+    intptr_t s1;    // Saved register
+    intptr_t a0;    // Function argument/return value
+    intptr_t a1;    // Function argument/return value
+    intptr_t a2;    // Function argument
+    intptr_t a3;    // Function argument
+    intptr_t a4;    // Function argument
+    intptr_t a5;    // Function argument
+    intptr_t a6;    // Function argument
+    intptr_t a7;    // Function argument
+    intptr_t s2;    // Saved register
+    intptr_t s3;    // Saved register
+    intptr_t s4;    // Saved register
+    intptr_t s5;    // Saved register
+    intptr_t s6;    // Saved register
+    intptr_t s7;    // Saved register
+    intptr_t s8;    // Saved register
+    intptr_t s9;    // Saved register
+    intptr_t s10;   // Saved register
+    intptr_t s11;   // Saved register
+    intptr_t t3;    // Temporary
+    intptr_t t4;    // Temporary
+    intptr_t t5;    // Temporary
+    intptr_t t6;    // Temporary
+    intptr_t epc;
+};
+
+
+
+
 extern void halt(xlen_t,xlen_t);
-extern void syscall_handler(xlen_t sp[]);
-extern xlen_t trap_handler(xlen_t mcause,xlen_t mepc,xlen_t sp[]);
+extern void syscall_handler(struct regfiles *sp);
+extern xlen_t trap_handler(xlen_t mcause,xlen_t mepc,struct regfiles *sp);
 
 enum syscall_num_t {SYS_EXIT=0,SYS_HALT,SYS_TASKSW,};
 
