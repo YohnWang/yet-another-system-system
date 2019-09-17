2f646de2100beb8b1793d10290eeb99d7110f4df
YohnWang <347220312@qq.com>
2019-09-17 21:01:17 +0800

    update syscall

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -19,9 +19,15 @@ long do_syscall(long a0,long a1,long a2,long a3,long a4,long a5,long n)
 	return f(a0,a1,a2,a3,a4,a5,n);
 }
 
-long syscall(long sysnum,...)
+long syscall(long sysnum,long a0,long a1,long a2,long a3,long a4,long a5)
 {
-	long r;
-	asm volatile ("ecall\n\t mv %[r],a0":[r]"=r"(r));
-	return r;
+	register long a0 asm("a0") = a0;
+	register long a1 asm("a1") = a1;
+	register long a2 asm("a2") = a2;
+	register long a3 asm("a3") = a3;
+	register long a4 asm("a4") = a4;
+	register long a5 asm("a5") = a5;
+	register long n  asm("a7") = sysnum;
+	asm volatile ("ecall":"+r"(a0):"r"(a0),"r"(a1),"r"(a2),"r"(a3),"r"(a4),"r"(a5),"r"(n));
+	return a0;
 }
